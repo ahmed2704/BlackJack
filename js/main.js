5cfdaa2c79b3e85411bfe637e62c9f25db6294fc
ahmed2704 <kassimahmed2704@gmail.com>
2024-08-05 16:53:59 -0400

    cache msg El

--- js/main.js
+++ js/main.js
@@ -1,14 +1,45 @@
-let dealerSum = 0;
-let playerSum = 0;
-let dAceCount = 0;
-let pAceCount = 0;
-let hidden;
+/*-------------------------------- Constants --------------------------------*/
+const suits = ["h", "c", "d", "s"];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const MSG_LOOKUP = {
+  null: 'Good Luck!',
+  'T': "It's a Push",
+  'P': 'Player Wins!',
+  'D': 'Dealer Wins',
+  'PBJ': 'Player Has Blackjack :smiley:',
+  'DBJ': 'Dealer Has Blackjack :pensive:',
+};
+// const mainDeck = buildMainDeck();
+/*---------------------------- Variables (state) ----------------------------*/
 let deck;
-let canHit = true;
-window.onload = function() {
+let pHand, dHand;
+let dTotal, pTotal; 
+let bankRoll , bet;
+let outcome;
+/*------------------------ Cached Element References ------------------------*/
+const confirmButton = document.querySelector("#confirm-bet");
+const inputBetEl = document.querySelector("#bet");
+const hitButton = document.querySelector("#hit");
+const standButton = document.querySelector("#stand");
+const amountBetEl = document.querySelector("#amount-bet");
+const bankEl = document.querySelector(".bank-amount");
+const msgEl = document.querySelector("#msg");
+/*----------------------------- Event Listeners -----------------------------*/
+inputBetEl.addEventListener("keypress", betPlaced)
+confirmButton.addEventListener("click", handleBet);
+// hitButton.addEventListener("click");
+// standButton.addEventListener("click");
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+init();
+
+function init() {
+  bank = 5000;
+  render();
   buildDeck();
   shuffleDeck();
-  //startGame();
 }
 function buildDeck() {
 deck = [];
@@ -44,5 +75,18 @@ function shuffleDeck(){
     deck[i] = deck[j];
     deck[j] = temp;
   }
-  console.log(deck)
+}
+
+async function betPlaced(evt){
+  console.log(evt.target.value)
+  amountBet = await evt.target.value 
+}
+console.log(amountBet)
+function handleBet(){
+  const btn = amountBet;
+  const amounBtetEl = amountBet;
+  
+  // when confirmButton clicked take placeBet amount and put it into amounBtetEl and subtracts placeBet amount from bankEl
+  // if player wins amountBetEl gets * 1.5 and placed back into bankEl
+  // if player loses 
 }
